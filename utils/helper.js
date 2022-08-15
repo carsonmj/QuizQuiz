672d1f02934ae14996b09ea9f984666022345537
eunoia2040 <eunoia2040@gmail.com>
2022-08-16 08:08:18 +0900

    feat: add helper functions

---
+++ utils/helper.js
@@ -0,0 +1,40 @@
+export const getURL = (path, param) => {
+  try {
+    if (typeof path !== "string") {
+      throw new Error("getURL Parameter 'path' should be string");
+    }
+
+    if (Object.keys(param)?.length === 0) {
+      return path;
+    }
+
+    const params = [];
+
+    for (const [key, value] of Object.entries(param)) {
+      if (value === "any") {
+        continue;
+      }
+
+      params.push(`${key}=${value}`);
+    }
+
+    return path.concat("?", params.join("&"));
+  } catch (error) {
+    console.error(error);
+  }
+
+};
+
+const shuffle = (array) => {
+  return array.sort(() => Math.random() - 0.5);
+};
+
+export const reOrganizeQuestions = (questions) => {
+  for (const question of questions) {
+    const { correct_answer, incorrect_answers } = question;
+    const options = [correct_answer, ...incorrect_answers];
+    question.options = shuffle(options);
+  }
+
+  return questions;
+};
