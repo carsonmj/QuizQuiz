809a70d7ff30107165656deca367267cac295576
eunoia2040 <eunoia2040@gmail.com>
2022-08-17 03:10:04 +0900

    refactor: update code style

--- components/button/HomeButton.js
+++ components/button/HomeButton.js
@@ -1,6 +1,6 @@
-import { MdHomeFilled } from 'react-icons/md';
+import { MdHomeFilled } from "react-icons/md";
 
-const HomeButton = ({ iconSize, onClick, children }) => {
+const HomeButton = ({ iconSize = "1.5rem", onClick, children }) => {
   return (
     <button
       type="button"
@@ -8,7 +8,7 @@ const HomeButton = ({ iconSize, onClick, children }) => {
       className="w-24 h-8 rounded-2xl bg-[#7d60f2] font-bold text-white"
     >
       <div className="flex flex-row justify-evenly">
-        <MdHomeFilled size={"1.5rem"} />
+        <MdHomeFilled size={iconSize} />
         {children}
       </div>
     </button>
