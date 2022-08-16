bf04a08e3a15877e20f3056ac616259418852379
eunoia2040 <eunoia2040@gmail.com>
2022-08-17 03:13:20 +0900

    feat: update next.config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,21 @@
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
+const intercept = require("intercept-stdout");
+
+// safely ignore recoil warning messages in dev (triggered by HMR)
+const interceptStdout = (text) => {
+  if (text.includes("Duplicate atom key")) {
+    return "";
+  }
+  return text;
+}
+
+if (process.env.NODE_ENV === "development") {
+  intercept(interceptStdout);
+}
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
