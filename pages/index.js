b2d4b144f3b871251138594b3e148e35be381354
eunoia2040 <eunoia2040@gmail.com>
2022-08-17 03:20:39 +0900

    feat: add data for production mode

--- pages/index.js
+++ pages/index.js
@@ -47,6 +47,14 @@ export const getStaticProps = async () => {
     if (process.env.NODE_ENV === "development") {
       const res = await fetch("http://localhost:3000/api/description");
       descriptions = await res.json();
+    } else {
+      descriptions = [
+        { id: 0, item: "Each time you will be given 10 questions on a random topic." },
+        { id: 1, item: "You can move on to the next question if you have selected an answer." },
+        { id: 2, item: "You can choose the answer to the question from among 4 options." },
+        { id: 3, item: "When you have completed all the questions, you can check the statistics of the questions." },
+        { id: 4, item: "You can solve the question again or check the review notes." },
+      ];
     }
   } catch (error) {
     return { props: { code: 500, result: [] } };
