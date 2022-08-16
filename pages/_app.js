9286255a6c7b573f2cd98468e374172a1f3beee6
eunoia2040 <eunoia2040@gmail.com>
2022-08-16 10:17:52 +0900

    feat: add recoil setting

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
+import { RecoilRoot } from "recoil";
 import "../styles/globals.css";
 
 const MyApp = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <RecoilRoot>
+      <Component {...pageProps} />
+    </RecoilRoot>
+  );
 };
 
 export default MyApp;
