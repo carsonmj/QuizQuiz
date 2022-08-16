800c8ce724a0238c833880843d07b5ebc433893e
eunoia2040 <eunoia2040@gmail.com>
2022-08-17 07:36:35 +0900

    chore: add cypress config

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: "http://localhost:3000",
+    setupNodeEvents(on, config) {
+      require('@cypress/code-coverage/task')(on, config)
+      return config
+    },
+  },
+});
