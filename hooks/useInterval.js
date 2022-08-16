e7c8e596acf19b2a4eafeb325c691494a76f5b80
eunoia2040 <eunoia2040@gmail.com>
2022-08-16 10:16:24 +0900

    feat: update useInterval

--- hooks/useInterval.js
+++ hooks/useInterval.js
@@ -1,7 +1,8 @@
-import { useEffect, useRef } from "react";
+import { useCallback, useEffect, useRef } from "react";
 
 const useInterval = (callback, delay) => {
   const savedCallback = useRef();
+  const intervalRef = useRef(null);
 
   useEffect(() => {
     savedCallback.current = callback;
@@ -10,13 +11,26 @@ const useInterval = (callback, delay) => {
   useEffect(() => {
     const tick = () => {
       savedCallback.current();
-    }
+    };
 
     if (delay !== null) {
       let id = setInterval(tick, delay);
+      intervalRef.current = id;
       return () => clearInterval(id);
     }
   }, [delay]);
+
+  useEffect(() => {
+    return () => clearInterval(intervalRef.current);
+  }, []);
+
+  const stop = useCallback(() => {
+    if (intervalRef.current !== null) {
+      clearInterval(intervalRef.current);
+    }
+  })
+
+  return { stop };
 };
 
 export default useInterval;
